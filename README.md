bb9fabf0df01d887ae5baf8ddc317a8bcb3fabce
mirmik <mirmikns@yandex.ru>
2019-04-03 23:12:53 +0300

    'README'

---
+++ README.md
@@ -0,0 +1,7 @@
+zencad-cli
+==========
+
+Utility for starting zencad GUI with  
+```zencad```  
+command instead  
+```python3 -m pip zencad```
--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ from wheel.bdist_wheel import bdist_wheel as bdist_wheel_
 from setuptools import setup, Extension, Command
 from distutils.util import get_platform
 
-import zencad
-
 import glob
 import sys
 import os
@@ -13,9 +11,9 @@ import os
 setup(
 	name = 'zencad-cli',
 	packages = ['zencad-cli'],
-	version = '0.1.1',
+	version = '0.1.2',
 	license='MIT',
-	description = 'CAD system for righteous zen programmers ',
+	description = 'Console line utility for zencad',
 	author = 'mirmik',
 	author_email = 'mirmikns@yandex.ru',
 	url = 'https://mirmik.github.io/zencad/',
